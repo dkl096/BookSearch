44a0589c5c6cb8e4ab45925a4bb44d02e4c12718
dkl096 <144052734+dkl096@users.noreply.github.com>
2024-10-13 02:16:05 +0800

    changed list layout to grid

--- src/app/components/BookList.tsx
+++ src/app/components/BookList.tsx
@@ -15,18 +15,34 @@ export default function BookList({books}: BookListProps){
     }
 
     return (
-        <ul>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-">
             {books.map((book) => (
-                <li key={book.id}>
-                    <div>
-                    <img src={book.volumeInfo.imageLinks?.smallThumbnail} alt={book.volumeInfo.title} />
+                <div key={book.id}>
+                    <div className = "cursor-pointer border p-4 rounded-md shadow-sm hover:shadow-xl transition-shadow duration-300">
+                        <img
+                            src={book.volumeInfo.imageLinks?.smallThumbnail}
+                            alt={book.volumeInfo.title}
+                            className="w-full h-48 object-contain mb-4"
+                        />
+                        <h3 className="text-lg font-semibold mt-2">{book.volumeInfo.title}</h3>
+                        <p className="text-sm text-gray-600">{book.volumeInfo.authors?.join(', ')}</p>
                     </div>
-                    <div>
-                    <h3>{book.volumeInfo.title}</h3>
-                    <p>{book.volumeInfo.authors?.join(', ')}</p>
-                    </div>
-                </li>
+                </div>
             ))}
-        </ul>
+        </div>
+
+        // <ul>
+        //     {books.map((book) => (
+        //         <li key={book.id}>
+        //             <div>
+        //             <img src={book.volumeInfo.imageLinks?.smallThumbnail} alt={book.volumeInfo.title} />
+        //             </div>
+        //             <div>
+        //             <h3>{book.volumeInfo.title}</h3>
+        //             <p>{book.volumeInfo.authors?.join(', ')}</p>
+        //             </div>
+        //         </li>
+        //     ))}
+        // </ul>
     );
 }
